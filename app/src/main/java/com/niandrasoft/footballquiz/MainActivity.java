9fe32006a0dffed99f3c573cdc332a4a713ea4f3
Nikolai Kanchev <Nikolai Kanchev>
2016-03-03 15:53:44 +0200

    Initial commit

---
+++ app/src/main/java/com/niandrasoft/footballquiz/MainActivity.java
@@ -0,0 +1,46 @@
+package com.niandrasoft.footballquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    Button clickButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        clickButton=(Button)findViewById(R.id.button);
+    }
+
+    public void clickMe(View view){
+        Toast.makeText(this, "Welcome in GenyMotion!",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
