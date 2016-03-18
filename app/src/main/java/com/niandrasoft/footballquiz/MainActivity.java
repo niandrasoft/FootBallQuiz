34a1d212caddbf96982377b77cf07cce5e9976a1
niandrasoft <niandrasoft@abv.bg>
2016-03-18 12:37:33 +0200

    welcome MainMenu

--- app/src/main/java/com/niandrasoft/footballquiz/MainActivity.java
+++ app/src/main/java/com/niandrasoft/footballquiz/MainActivity.java
@@ -3,11 +3,9 @@ package com.niandrasoft.footballquiz;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
     Button clickButton;
