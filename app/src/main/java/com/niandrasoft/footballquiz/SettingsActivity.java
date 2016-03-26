5a934c2b597e16a68b77c6580c16575bcaf4e8f8
rdatanasov <rdatanasov@gmail.com>
2016-03-26 16:04:40 +0200

    create a sound settings

--- app/src/main/java/com/niandrasoft/footballquiz/SettingsActivity.java
+++ app/src/main/java/com/niandrasoft/footballquiz/SettingsActivity.java
@@ -1,23 +1,52 @@
 package com.niandrasoft.footballquiz;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class SettingsActivity extends Activity implements View.OnClickListener{
+
+    //private Boolean b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+
+        CheckBox box = (CheckBox) findViewById(R.id.check1);
+
+        if(isServiceRunning()){
+            box.setChecked(true);
+        }else{
+            box.setChecked(false);
+        }
+
+
     }//end of onCreate method
+
+
+    private boolean isServiceRunning() {
+        ActivityManager manager = (ActivityManager)getSystemService(ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if ("com.niandrasoft.footballquiz.SoundService".equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public void onClick(View v) {
     }
+
+
 
 
     public void onClickBack(View view) {
@@ -30,4 +59,49 @@ public class SettingsActivity extends Activity implements View.OnClickListener{
         startActivity(i);
 
     }
-}
+
+    public void onOffMusic(View view) {
+        Boolean b = ((CheckBox) findViewById(R.id.check1)).isChecked();
+        if(b == true){
+            Intent svc = new Intent(SettingsActivity.this, SoundService.class);
+            startService(svc);
+        }else{
+            Intent svc = new Intent(SettingsActivity.this, SoundService.class);
+            stopService(svc);
+        }
+    }//end of onOffMusic method
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Boolean check = ((CheckBox) findViewById(R.id.check1)).isChecked();
+
+        SharedPreferences pref = getSharedPreferences("SettingsActivity", MODE_PRIVATE);
+        SharedPreferences.Editor edit = pref.edit();
+        edit.putBoolean("checkbox1", check);
+        edit.commit();
+    }//end of onPause
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences pref = getSharedPreferences("SettingsActivity", MODE_PRIVATE);
+        Boolean bb = pref.getBoolean("checkbox1", false);
+        if (bb) {
+            if(bb!=false){
+                CheckBox box = (CheckBox) findViewById(R.id.check1);
+                box.setChecked(true);
+            }else{
+                CheckBox box = (CheckBox) findViewById(R.id.check1);
+                box.setChecked(false);
+            }
+        }
+    }//end of onResume
+
+
+
+
+}//end of class
